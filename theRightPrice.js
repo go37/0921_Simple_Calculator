052a5cf0ec6f1ab20a79e9b5eee30a54f0e0c059
JennyViannay <jenny.viannay75@gmail.com>
2021-09-23 17:44:39 +0200

    fix code

---
+++ theRightPrice.js
@@ -0,0 +1,19 @@
+const userName = prompt('Give your name :');
+if (userName) {
+    const random = Math.floor(Math.random() * 100) + 1;
+    let userNumber = prompt('Choose number between 1 & 100 :');
+    while (parseInt(userNumber) !== random) {
+        if (userNumber < random) {
+            console.log('It\'s more');
+            userNumber = prompt('Choose number up to :' + userNumber);
+        }
+        if (userNumber > random) {
+            console.log('It\'s less');
+            userNumber = prompt('Choose number under to :' + userNumber);
+        }
+        if (parseInt(userNumber) === random) {
+            alert('You win !')
+        }
+        start++;
+    }
+}
